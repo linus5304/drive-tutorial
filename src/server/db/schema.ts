d4cf800cd7d21df1ed98c4fd94f070de0d0195b3
linusbayere <linusbayere@gmail.com>
2025-02-01 14:55:47 -0500

    db model working!

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,25 @@
-import { int, singlestoreTable, varchar } from 'drizzle-orm/singlestore-core';
+import { bigint, index, int, singlestoreTableCreator, text } from 'drizzle-orm/singlestore-core';
 
-export const usersTable = singlestoreTable('users_table', {
-  id: int().primaryKey(),
-  name: varchar({ length: 255 }).notNull(),
-  age: int().notNull()
+export const createTable = singlestoreTableCreator((name) => `drive-tutorial_${name}`);
+
+export const filesTable = createTable('files_table', {
+  id: bigint("id", { mode: 'number', unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  url: text("url").notNull(),
+  parent: bigint("parent", { mode: 'number', unsigned: true }).notNull(),
+  size: int("size").notNull()
+}, (t) => {
+  return [
+    index("parent_index").on(t.parent)
+  ]
+});
+
+export const foldersTable = createTable('folders_table', {
+  id: bigint("id", { mode: 'number', unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: bigint("parent", { mode: 'number', unsigned: true }),
+}, (t) => {
+  return [
+    index("parent_index").on(t.parent)
+  ]
 });
