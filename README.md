6ccae11685a1dbe917baf899c1d4672c1d2cc311
linusbayere <linusbayere@gmail.com>
2025-02-01 18:59:31 -0500

    fix order

--- README.md
+++ README.md
@@ -6,14 +6,6 @@
 - [x] Move folder open state to URL
 - [x] Add auth
 - [x] Add file uploading
-- [ ] Add analytics (posthog)
+- [x] Add analytics (posthog)
 - [ ] Add delete
 - [ ] Real homepage + onboarding
-
-## Notes from 01/02/2025
-
-- [x] Add ownweship to files and folders
-- [x] Upload files to the right folder
-- [x] Allow files that aren't images to be uploaded
-- [ ] Delete files button
-- [ ] Real homepage
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -6,10 +6,10 @@ import { eq } from "drizzle-orm"
 
 export const QUERIES = {
     getFiles: async (folderId: number) => {
-        return db.select().from(filesTable).where(eq(filesTable.parent, folderId))
+        return db.select().from(filesTable).where(eq(filesTable.parent, folderId)).orderBy(filesTable.id)
     },
     getFolders: async (folderId: number) => {
-        return db.select().from(foldersTable).where(eq(foldersTable.parent, folderId))
+        return db.select().from(foldersTable).where(eq(foldersTable.parent, folderId)).orderBy(foldersTable.id)
     },
     getAllParentsForFolder: async (folderId: number) => {
         const parents = []
